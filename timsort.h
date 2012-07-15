7e7c5229bbc397922f8d45d5e933767b31b1c551
Victoria Wu <victoria.chen.wu@gmail.com>
2012-07-14 22:11:25 -0500

    Method template

--- timsort.h
+++ timsort.h
@@ -1,9 +1,17 @@
 #include <iostream>
-
-
-
-template <class T>
-class timsort
+#include <vector>
+#include <stack>
+#include <utility>
+using namespace std;
+/*
+PRE: Array 'data' is full in at least the first ArrSize 
+locations (0 to ArrSize-1, as usual). The data type is comparable, 
+with all  six comparison operators (< > <= >= == != ) defined. 
+POST: Data array is sorted into ascending (smallest to largest) 
+order.
+*/
+template <typename T>
+void Sort(T data[], int ArrSize) 
 {
 
 };
